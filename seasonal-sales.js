72274557def262e8e979b17886058607053e1388
Matt Hensley <matt.hensley777@gmail.com>
2017-03-23 15:53:14 -0500

    update

--- seasonal-sales.js
+++ seasonal-sales.js
@@ -1,15 +1,14 @@
-var productContainer = document.getElementById("productContainer"); 
+var productContainer = document.getElementById("productContainer");
 
-    function productDom(xhrData) {
-        var productString = "";
-        var currentProduct;
-        for (var i = 0; i < xhrData.products.length; i++) {
+function productDom(xhrData) {
+    var productString = "";
+    var currentProduct;
+    for (var i = 0; i < xhrData.products.length; i++) {
         currentProduct = xhrData.products[i];
 
         productString += `<div class="col-sm-6 col-md-4">`;
-        productString += `<h3>${currentProduct.name}</h3>`;
-        productString += `<p>is a ${currentProduct.price}</p>`;
-        // productString += `<p>is a ${currentProduct.category_id}</p>`;
+        productString += `<h3><br><br>${currentProduct.name}<br></h3>`;
+        productString += `<p>${currentProduct.price}</p>`;
         productString += `</div></div></div>`;
 
         productContainer.innerHTML = productString;
@@ -17,46 +16,89 @@ var productContainer = document.getElementById("productContainer");
     };
 }
 
-var departmentContainer = document.getElementById("departmentContainer"); 
+var departmentContainer = document.getElementById("departmentContainer");
 
-    function departmentDom(xhrData) {
-        var departmentString = "";
-        var currentDepartment;
-        for (var j = 0; j < xhrData.categories.length; j++) {
+function departmentDom(xhrData) {
+    var departmentString = "";
+    var currentDepartment;
+    for (var j = 0; j < xhrData.categories.length; j++) {
         currentDepartment = xhrData.categories[j];
 
         departmentString += `<div class="col-sm-6 col-md-4">`;
-        departmentString += `<h3>${currentDepartment.name}</h3>`;
-        departmentString += `<p>is a ${currentDepartment.season_discount}</p>`;
-        departmentString += `<p>is a ${currentDepartment.discount}</p>`;
+        departmentString += `<h1>${currentDepartment.name}</h1>`;
+        departmentString += `<p><button class="discount"id="discount-${currentDepartment.season_discount}">${currentDepartment.season_discount}</button></p>`;
+        // departmentString += `<p>Press for discount ${currentDepartment.discount}</p>`;
         departmentString += `</div></div></div>`;
 
         departmentContainer.innerHTML = departmentString;
+        // console.log(currentDepartment);
 
     };
+    discountInfo();
 }
 
 function useIfWorks() {
 
     var data = JSON.parse(this.responseText);
     productDom(data);
-    console.log(xhrData);
+
 }
 
-function useIfDoesntWork() {
 
+
+function useForDepartment() {
+
+    var data = JSON.parse(this.responseText);
+    departmentDom(data);
 
 }
 
 
 var myRequest = new XMLHttpRequest();
-myRequest.addEventListener("load", useIfWorks);
-myRequest.addEventListener("error", useIfDoesntWork);
+myRequest.addEventListener("load", useForDepartment);
 myRequest.open("GET", "department.json");
 myRequest.send();
 
 var myRequestTwo = new XMLHttpRequest();
 myRequestTwo.addEventListener("load", useIfWorks);
-myRequestTwo.addEventListener("error", useIfDoesntWork);
 myRequestTwo.open("GET", "main.json");
 myRequestTwo.send();
+
+function discountInfo() {
+
+var sDiscount = document.getElementsByClassName("discount");
+for (var i = 0; i < sDiscount.length; i++) {
+sDiscount[i].addEventListener("click", seasonalDiscount);
+}
+
+    function seasonalDiscount(season) {
+        if (season.value === winter);
+        console.log(season);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
